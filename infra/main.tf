bf40bc57c032348bfe03d466f1af2e1ae01082dc
h4ndzdatm0ld <hugotinoco@icloud.com>
2022-11-20 08:16:50 +0000

    main

--- infra/main.tf
+++ infra/main.tf
@@ -24,28 +24,32 @@ resource "aws_ecr_repository" "ansible_eda" {
   }
 }
 
+
 resource "aws_apprunner_service" "app_ansible_eda" {
   service_name = "aws-app-runner-ansible-eda"
 
   source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner-service-role.arn
+    }
     image_repository {
       image_configuration {
-        port = "5000"
+        port          = "5000"
         start_command = "docker run ${aws_ecr_repository.ansible_eda.repository_url}/${aws_ecr_repository.ansible_eda.name}:latest --rulebook rulebooks/rb-webhook-5000.yml -i inventories/dev/hosts.yml --verbose"
       }
       image_identifier      = "${aws_ecr_repository.ansible_eda.repository_url}/${aws_ecr_repository.ansible_eda.name}:latest"
-      image_repository_type = "ECR_PUBLIC"
+      image_repository_type = "ECR"
     }
-    auto_deployments_enabled = false
+    auto_deployments_enabled = true
   }
   health_check_configuration {
-          healthy_threshold   = 1
-          interval            = 10
-          path                = "/"
-          protocol            = "TCP"
-          timeout             = 5
-          unhealthy_threshold = 5
-        }
+    healthy_threshold   = 1
+    interval            = 10
+    path                = "/"
+    protocol            = "TCP"
+    timeout             = 5
+    unhealthy_threshold = 5
+  }
   tags = {
     Name = "ansible-eda"
   }
