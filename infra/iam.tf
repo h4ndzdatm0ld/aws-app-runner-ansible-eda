f5d83252f8a7f4563b8e09f14a6bd617d3df2b16
h4ndzdatm0ld <hugotinoco@icloud.com>
2022-11-21 04:31:45 +0000

    IAM

---
+++ infra/iam.tf
@@ -0,0 +1,43 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# APPRUNNER IAM Role
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_iam_role" "apprunner-service-role" {
+  name               = "${var.apprunner-service-role}AppRunnerECRAccessRole"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.apprunner-service-assume-policy.json
+}
+
+data "aws_iam_policy_document" "apprunner-service-assume-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["build.apprunner.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner-service-role-attachment" {
+  role       = aws_iam_role.apprunner-service-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+
+resource "aws_iam_role" "apprunner-instance-role" {
+  name = "${var.apprunner-service-role}AppRunnerInstanceRole"
+  path = "/"
+  assume_role_policy = data.aws_iam_policy_document.apprunner-instance-assume-policy.json
+}
+
+data "aws_iam_policy_document" "apprunner-instance-assume-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["tasks.apprunner.amazonaws.com"]
+    }
+  }
+}
